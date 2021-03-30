78c967e0082790927c373afc3e5fce17fe70ea88
Alex Karpovich <akarpovich@b2broker.net>
2021-03-30 11:40:13 +0300

    clean html

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: true,
+        dots: false,
+        slidesToShow: 1,
+        autoplay:false,
+        speed:1000,
+        autoplaySpeed:800,
+    });
+});
+
